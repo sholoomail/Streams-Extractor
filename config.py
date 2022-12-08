8068db6b7c765386dd07f2d6008cd1b5fcb49a17
sholoomail <84216658+sholoomail@users.noreply.github.com>
2022-12-08 16:56:34 +0330

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 
-    # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    # Get from my.telegram.org (or @UseTGXBot)    
+    API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
 
 
